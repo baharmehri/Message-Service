42bb2ddee04032cb7432e37c926614557d73165e
baharmehri <bahar.mehri.7180@gmail.com>
2025-06-02 14:55:02 +0330

    refactor: remove UserProfile model and consolidate fields into User

--- apps/users/models.py
+++ apps/users/models.py
@@ -9,7 +9,10 @@ from .roles import UserRoles
 class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
 
     objects = UserManager()
 
@@ -37,13 +40,3 @@ class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     @property
     def is_admin(self):
         return self.has_role(UserRoles.ADMIN)
-
-
-class Profile(BaseModel):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100, null=True, blank=True)
-    last_name = models.CharField(max_length=100, null=True, blank=True)
-    phone_number = models.CharField(max_length=11, null=True, blank=True)
-
-    def __str__(self):
-        return f'User ({self.user.username})\'s profile'
